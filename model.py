96e2cd9507c5b31c2ddef21300da65a92ddb002f
avyan-k <avyan.kandasamy@gmail.com>
2024-01-27 20:51:49 -0500

    Started Discriminator Model

--- model.py
+++ model.py
@@ -4,13 +4,62 @@ import keras
 from keras import layers
 from utils import FREQUENCIES as FREQUENCIES
 from utils import TIMES as TIMES
+GENERATEDTIMES = 100
 FREQUENCYKERNELSIZE = 5
 TIMEKERNELSIZE = 2
 GENERATORFILTERS = 1
-def generate_generator():
+
+def make_generator():
     inputs = keras.Input(shape=(FREQUENCIES,TIMES))
     frequency_convolution_list = list() 
     for _ in range(TIMES):
         frequency_convolution_list.append(tf.conv1D(filters = GENERATORFILTERS, kernel_size = FREQUENCYKERNELSIZE,strides=1,padding = 'same',activation='RELU')(inputs))
     concatenate_frequencies = tf.keras.layers.Concatenate()(frequency_convolution_list)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def make_discriminator():
+    inputs = inputs
