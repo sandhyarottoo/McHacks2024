2f440ff0521c612d5240ecf594629a298a6380ab
FrancoDB <francodbalso@gmail.com>
2024-01-27 21:26:43 -0500

    changed glob vars to being make_generator args

--- model.py
+++ model.py
@@ -4,37 +4,40 @@ import keras
 from keras import layers
 from utils import FREQUENCIES as FREQUENCIES
 from utils import TIMES as TIMES
-GENERATEDTIMES = 100
 
-FREQUENCYKERNELSIZE = 5
-TIMEKERNELSIZE = 2
-GENERATORFILTERS = 1
 
-def make_generator():
-    inputs = keras.Input(shape=(FREQUENCIES,TIMES))
+def make_generator(generator_filters = 1, generated_times = 100, frequency_kernel_size = 5, time_kernel_size = 2):
+
     inputs = keras.Input(shape=(TIMES, FREQUENCIES))
     outputs = inputs
 
-    frequency_convolution_list = list() 
+    for i in range(generated_times):
+        counter = 0
 
-    for i in range(TIMES):
-        
-        get_row = tf.keras.layers.Lambda(lambda x: x[:, i:i+1, :])(input_layer)
-        # Reshape row to match Conv1D requirements (shape will be (4000, 1))
-        reshape_row = tf.keras.layers.Reshape((input_shape[1], 1))(get_row)
+        # run the 1D convs along the TIMES most recent rows 
+        frequency_convolution_list = list() 
 
-        tf.conv1D(filters = GENERATORFILTERS, kernel_size = FREQUENCYKERNELSIZE,
-                                                    strides=1,padding = 'same',activation='RELU')(inputs)
+        for j in range(counter, TIMES):
+            # Get a row then reshape it to match Conv1D requirements (shape will be (4000, 1))
+            get_row = tf.keras.layers.Lambda(lambda x: x[:, j:j+1, :])(outputs)
+            reshaped_row = tf.keras.layers.Reshape((FREQUENCIES, 1))(get_row)
+            frequency_convolution_list.append(tf.conv1D(filters = generator_filters, kernel_size = frequency_kernel_size,
+                                                        strides=1,padding = 'same', activation='RELU')(reshaped_row))
+            
+        concatenate_frequencies = tf.keras.layers.Concatenate()(frequency_convolution_list)
 
-        frequency_convolution_list.append()
-        
-        
-        
-        # Create a Conv1D layer for each row
-        # Adjust filters, kernel_size, and activation according to your needs
-        conv_layer = tf.keras.layers.Conv1D(filters=64, kernel_size=3, padding='valid', activation='relu')(reshape_row)
+        # run the 1D convs along the columns of the TIMES most recent rows
+        time_convolution_list = list()
 
-    concatenate_frequencies = tf.keras.layers.Concatenate()(frequency_convolution_list)
+        for j in range(counter, TIMES):
+            get_row = tf.keras.layers.Lambda(lambda x: x[:, j:j+1, :])(outputs)
+            reshaped_row = tf.keras.layers.Reshape((FREQUENCIES, 1))(get_row)
+            frequency_convolution_list.append(tf.conv1D(filters = generator_filters, kernel_size = frequency_kernel_size,
+                                                        strides=1,padding = 'same', activation='RELU')(reshaped_row))
+
+
+
+        counter += 1
 
 
 
