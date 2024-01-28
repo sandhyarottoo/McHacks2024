5aa712897655d4b1e2ce4a29742f5fbffe2a223e
FrancoDB <francodbalso@gmail.com>
2024-01-28 11:06:30 -0500

    made the generator only output the input plus 2

--- model.py
+++ model.py
@@ -12,7 +12,8 @@ TIMES = 10
 
 
 def make_generator(freq_filters = 1, time_filters = 1, generated_times = 2, freq_kernel_size = 5, 
-                   time_kernel_size = 2, new_layers = 5):
+                   time_kernel_size = 2, new_layers = 2):
+    TIMES = 10
     inputs = keras.Input(shape=(TIMES, FREQUENCIES))
     outputs = inputs
 
@@ -28,45 +29,49 @@ def make_generator(freq_filters = 1, time_filters = 1, generated_times = 2, freq
         time_kernels.append(layers.Conv1D(filters = time_filters, kernel_size = time_kernel_size,
                                                         strides=1, padding = 'same', activation='relu'))
 
-    for counter in range(0, generated_times, new_layers):
+    row_reshaper = layers.Reshape((FREQUENCIES, 1))
+    final_reshaper = layers.Reshape((new_layers, FREQUENCIES))
+    flattener = layers.Flatten()
+    ax1_concat = layers.Concatenate(axis=1)
 
-        # run the 1D convs along the TIMES most recent rows 
-        freq_conv_output_list = list() 
+    # run the 1D convs along the TIMES most recent rows 
+    freq_conv_output_list = list() 
 
-        for j in range(counter, TIMES + counter):
-            # Get a row then reshape it to match Conv1D requirements (shape will be (4000, 1))
-            get_row = layers.Lambda(lambda x: x[:, j, :])(outputs)
-            reshaped_row = layers.Reshape((FREQUENCIES, 1))(get_row)
-            assert reshaped_row.shape[1:] == (FREQUENCIES, 1)
+    for j in range(0, TIMES):
+        # Get a row then reshape it to match Conv1D requirements (shape will be (4000, 1))
+        get_row = outputs[:, j, :]
+        reshaped_row = row_reshaper(get_row)
+        assert reshaped_row.shape[1:] == (FREQUENCIES, 1)
 
-            freq_conv_output_list.append(freq_kernels[j-counter](reshaped_row))
-            
-        concatenated_frequencies = layers.Concatenate(axis=1)(freq_conv_output_list)
-        concatenated_frequencies = layers.Flatten()(concatenated_frequencies)
+        freq_conv_output_list.append(freq_kernels[j](reshaped_row))
+        
+    concatenated_frequencies = ax1_concat(freq_conv_output_list)
+    concatenated_frequencies = flattener(concatenated_frequencies)
 
-        # run the 1D convs along the columns of the TIMES most recent rows
-        time_conv_output_list = list()
+    # run the 1D convs along the columns of the TIMES most recent rows
+    time_conv_output_list = list()
 
-        for j in range(FREQUENCIES):
-            get_column = tf.expand_dims(layers.Lambda(lambda x: x[:, counter:TIMES+counter, j])(outputs), axis=-1)
-            assert get_column.shape[1:] == (TIMES, 1)
+    for j in range(FREQUENCIES):
+        get_column = tf.expand_dims(outputs[:, 0:TIMES, j], axis=-1)
+        assert get_column.shape[1:] == (TIMES, 1)
 
-            time_conv_output_list.append(time_kernels[j//10](get_column))
+        time_conv_output_list.append(time_kernels[j//10](get_column))
 
-        concatenated_times = layers.Concatenate(axis=1)(time_conv_output_list)
-        concatenated_times = layers.Flatten()(concatenated_times)
+    concatenated_times = ax1_concat(time_conv_output_list)
+    concatenated_times = flattener(concatenated_times)
 
-        # get the first vector that will be sent to the dense portion
-        vector = layers.Concatenate(axis=1)([concatenated_frequencies, concatenated_times])
-        assert vector.shape[1:] == (TIMES*FREQUENCIES*(freq_filters + time_filters))
+    # get the first vector that will be sent to the dense portion
+    vector = ax1_concat([concatenated_frequencies, concatenated_times])
+    
+    assert vector.shape[1:] == (TIMES*FREQUENCIES*(freq_filters + time_filters))
 
-        vector = layers.Dense(units=TIMES*FREQUENCIES*(freq_filters + time_filters)//2, activation='relu')(vector)
-        vector = layers.Dense(units=TIMES*FREQUENCIES*freq_filters//4, activation='relu')(vector)
-        new_rows = layers.Dense(units=FREQUENCIES*new_layers, activation='relu')(vector)
-        new_rows = layers.Reshape((new_layers, FREQUENCIES))(new_rows)
+    vector = layers.Dense(units=8000, activation='relu')(vector)
+    vector = layers.Dense(units=6000, activation='relu')(vector)
+    new_rows = layers.Dense(units=FREQUENCIES*new_layers, activation='relu')(vector)
+    new_rows = final_reshaper(new_rows)
 
-        outputs = layers.Concatenate(axis=1)([outputs, new_rows])
-        assert outputs.shape[1:] == (TIMES + (counter+1)*new_layers, FREQUENCIES)
+    outputs =  ax1_concat([outputs, new_rows])
+    assert outputs.shape[1:] == (TIMES + new_layers, FREQUENCIES)
     
     model = keras.Model(inputs=inputs, outputs=outputs)
 
@@ -85,7 +90,7 @@ def make_full_generator():
     real_part = tf.expand_dims(re_gen(tensor1), axis=0)
     im_part = tf.expand_dims(im_gen(tensor2), axis=0)
 
-    combined = layers.Concatenate()([real_part, im_part])
+    combined = layers.Concatenate(axis=1)([real_part, im_part])
 
     model = keras.Model(inputs=inputs, outputs=combined)
 
@@ -118,5 +123,5 @@ def make_discriminator(initial_filters = 64,number_convolution_layers=2,neurons_
 
 if __name__ == '__main__':
     critic = make_discriminator(initial_filters = 32,number_convolution_layers=2,neurons_per_dense_layer=128,number_dense_layers=3)
-    generator = make_full_generator()
+    generator = make_generator()
     gan = make_GAN(generator=generator,discriminator=critic)
