0bf087f607681b647f8a0e3719893be7cfae37dc
avyan-k <avyan.kandasamy@gmail.com>
2024-01-27 19:53:50 -0500

    set frequencies and times

---
+++ model.py
@@ -0,0 +1,16 @@
+import numpy as np
+import tensorflow as tf
+import keras
+from keras import layers
+from utils import FREQUENCIES as FREQUENCIES
+from utils import TIMES as TIMES
+FREQUENCYKERNELSIZE = 5
+TIMEKERNELSIZE = 2
+GENERATORFILTERS = 1
+def generate_generator():
+    inputs = keras.Input(shape=(FREQUENCIES,TIMES))
+    frequency_convolution_list = list() 
+    for _ in range(TIMES):
+        frequency_convolution_list.append(tf.conv1D(filters = GENERATORFILTERS, kernel_size = FREQUENCYKERNELSIZE,strides=1,padding = 'same',activation='RELU')(inputs))
+    concatenate_frequencies = tf.keras.layers.Concatenate()(frequency_convolution_list)
+
